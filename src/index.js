797d710d1254f2d7cc7cc8602e84ef6593ed7b69
Dima <tipanswer@gmail.com>
2021-07-07 18:44:17 +0300

    Bomberman

--- src/index.js
+++ src/index.js
@@ -1,16 +1,219 @@
 import './styles.scss'
+import img from './Bomberman.png'
+
+const loadSprite = (sprite) => {
+    const image = new Image()
+    image.src = sprite
+    return image
+}
+
+const sprite = loadSprite(img)
+
+const WIDTH = 300
+const HEIGHT = 300
+const DPI_WIDTH = WIDTH * 1.2
+const DPI_HEIGHT = HEIGHT * 1.2
+const defaultSizeSprite = 16
+
+const arrowUp = 'ArrowUp'
+const arrowDown = 'ArrowDown'
+const arrowLeft = 'ArrowLeft'
+const arrowRight = 'ArrowRight'
+
+const CONCRETE_WALL = 'CONCRETE_WALL'
+const BRICK_WALL = 'BRICK_WALL'
+
+const FIELD = []
+
+const BLOCK_SIZE = 27
+
+const BLOCKS_X = Math.round(DPI_WIDTH / BLOCK_SIZE)
+const BLOCKS_Y = Math.round(DPI_HEIGHT / BLOCK_SIZE)
+
+const LENGTH_X = BLOCKS_X - 1
+const LENGTH_Y = BLOCKS_Y - 1
 
 const canvas = document.querySelector('[data-el="main"]');
 const ctx = canvas.getContext('2d')
 
-const WIDTH = 600
-const HEIGHT = 200
-
 canvas.style.width = WIDTH + 'px'
 canvas.style.height = HEIGHT + 'px'
 
-canvas.width = WIDTH
-canvas.height = HEIGHT
+canvas.width = DPI_WIDTH
+canvas.height = DPI_HEIGHT
 
-const BLOCK_SIZE = 20
+const playerUp    = animation([[48, 16], [79.5, 16]], 10)
+const playerDown  = animation([[48, 0], [79.5, 0]], 10)
+const playerRight = animation([[0, 16], [32, 16]], 10)
+const playerLeft  = animation([[0, 0], [32, 0]], 10)
 
+const player = {
+    x: BLOCK_SIZE + 2,
+    y: BLOCK_SIZE + 2,
+    step: 3,
+    move: false,
+    direction : arrowRight,
+
+    movement() {
+        if (!this.move) {
+            switch (this.direction) {
+                case arrowUp:
+                    ctx.drawImage(sprite, 64, 16, defaultSizeSprite, defaultSizeSprite, player.x, player.y, BLOCK_SIZE - 2, BLOCK_SIZE - 2)
+                    break
+                case arrowDown:
+                    ctx.drawImage(sprite, 66, 0, defaultSizeSprite, defaultSizeSprite, player.x, player.y, BLOCK_SIZE - 2, BLOCK_SIZE - 2)
+                    break
+                case arrowLeft:
+                    ctx.drawImage(sprite, 16, 0, defaultSizeSprite, defaultSizeSprite, player.x, player.y, BLOCK_SIZE - 2, BLOCK_SIZE - 2)
+                    break
+                case arrowRight:
+                    ctx.drawImage(sprite, 16, 16, defaultSizeSprite, defaultSizeSprite, player.x, player.y, BLOCK_SIZE - 2, BLOCK_SIZE - 2)
+                    break
+            }
+
+        } else {
+            switch (this.direction) {
+                case arrowUp: {
+                    const [x, y] = playerUp()
+                    ctx.drawImage(sprite, x, y, defaultSizeSprite, defaultSizeSprite, player.x, player.y, BLOCK_SIZE - 2, BLOCK_SIZE - 2)
+                    break
+                }
+                case arrowDown: {
+                    const [x, y] = playerDown()
+                    ctx.drawImage(sprite, x, y, defaultSizeSprite, defaultSizeSprite, player.x, player.y, BLOCK_SIZE - 2, BLOCK_SIZE - 2)
+                    break
+                }
+                case arrowLeft: {
+                    const [x, y] = playerLeft()
+                    ctx.drawImage(sprite, x, y, defaultSizeSprite, defaultSizeSprite, player.x, player.y, BLOCK_SIZE - 2, BLOCK_SIZE - 2)
+                    break
+                }
+                case arrowRight: {
+                    const [x, y] = playerRight()
+                    ctx.drawImage(sprite, x, y, defaultSizeSprite, defaultSizeSprite, player.x, player.y, BLOCK_SIZE - 2, BLOCK_SIZE - 2)
+                    break
+                }
+            }
+        }
+    }
+}
+
+function clear() {
+    ctx.clearRect(0, 0, DPI_WIDTH, DPI_HEIGHT)
+}
+
+function setupField() {
+    for (let y = 0; y < BLOCKS_Y; y++ ) {
+        FIELD[y] = []
+        for (let x = 0; x < BLOCKS_X; x++) {
+            FIELD[y].push('')
+
+            if (y === 0) {
+                FIELD[y][x] = CONCRETE_WALL
+            }
+            else if(x === 0) {
+                FIELD[y][x] = CONCRETE_WALL
+            }
+            else if (x === LENGTH_X) {
+                FIELD[y][x] = CONCRETE_WALL
+            }
+            else if( y === LENGTH_Y) {
+                FIELD[y][x] = CONCRETE_WALL
+            }
+
+            if (y % 2 === 0 && x % 2 === 0) {
+                FIELD[y][x] = CONCRETE_WALL
+            }
+        }
+    }
+}
+
+function drawField() {
+    for (let y = 0; y < FIELD.length; y++ ) {
+        for (let x = 0; x < FIELD[y].length; x++) {
+            drawImage(0, 70, x, y)
+
+            switch (FIELD[y][x]) {
+                case CONCRETE_WALL:
+                    drawImage(48, 48, x, y)
+                    break
+            }
+        }
+    }
+}
+
+function animation(frames, loop = 6) {
+    let frameIndex = 0
+    let tickCount = 0
+
+    return () => {
+        tickCount++
+
+        if (tickCount > loop) {
+            tickCount = 0;
+            if (frameIndex < frames.length - 1) {
+                frameIndex++;
+            } else {
+                frameIndex = 0;
+            }
+        }
+
+        return frames[frameIndex]
+    }
+}
+
+function render() {
+    clear()
+
+    setupField()
+    drawField()
+
+    player.movement()
+
+    requestAnimationFrame(render)
+}
+
+render()
+
+function drawImage(sx, sy, dx, dy) {
+   ctx.drawImage(
+       sprite,
+       sx,
+       sy,
+       defaultSizeSprite,
+       defaultSizeSprite,
+       dx * BLOCK_SIZE,
+       dy * BLOCK_SIZE,
+       BLOCK_SIZE,
+       BLOCK_SIZE
+   )
+}
+
+document.addEventListener('keydown', listenerKeyDown)
+document.addEventListener('keyup', listenerKeyUp)
+
+function listenerKeyUp() {
+    player.move = false
+}
+
+function listenerKeyDown(e) {
+    e.preventDefault();
+
+    player.move = true
+    player.direction = e.code
+
+    switch(e.code) {
+        case arrowUp:
+            player.y -= player.step
+            break
+        case arrowDown:
+            player.y += player.step
+            break
+        case arrowLeft:
+            player.x -= player.step
+            break
+        case arrowRight:
+            player.x += player.step
+            break
+    }
+}
